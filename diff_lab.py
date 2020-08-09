0d91d5786d87648d2f40a66c28e18edf4ba3414c
tinalimbu <tinalawati@gmail.com>
2020-08-09 16:52:08 +0100

    Improved documentation

--- diff_lab.py
+++ diff_lab.py
@@ -15,9 +15,6 @@ class Extensions(Enum):
 
 SUPPORTED_EXTENSIONS = [Extensions.XLSX.value, Extensions.CSV.value]
 FONT = ("helvetica", 12, "bold")
-FILE_1_NAME = "Name of first file:"
-FILE_2_NAME = "Name of second file:"
-DIFF = "DIFF"
 CURRENT_PATH = os.getcwd()
 
 
@@ -25,37 +22,41 @@ class Differ:
 	def __init__(self, root):
 		self.root = root
 
+		# Main frame.
 		self.frame = tk.Frame(self.root)
 
+		# Description.
 		self.description = tk.Label(
 			self.frame, text=self.get_description(), font=FONT
 		).grid(row=0, padx=40, pady=40)
 
+		# First file input.
 		self.file_1_label = tk.Label(
-			self.frame, text=FILE_1_NAME, font=FONT
+			self.frame, text="Name of first file:", font=FONT
 		).grid(row=1, pady=10)
-
 		self.file_1_value = tk.StringVar(root)
 		self.file_1_input = tk.Entry(
 			self.frame, textvariable=self.file_1_value
 		).grid(row=2)
 
+		# Second file input.
 		self.file_2_label = tk.Label(
-			self.frame, text=FILE_2_NAME, font=FONT
+			self.frame, text="Name of second file:", font=FONT
 		).grid(row=3, pady=10)
-
 		self.file_2_value = tk.StringVar(root)
 		self.file_2_input = tk.Entry(
 			self.frame, textvariable=self.file_2_value
 		).grid(row=4)
 
+		# Diff button.
 		self.diff_button = tk.Button(
-			self.frame, text=DIFF, command=self.diff_files, font=FONT
+			self.frame, text="DIFF", command=self.diff_files, font=FONT
 		).grid(row=5, pady=50)
 
 		self.frame.pack()
 
 	def get_description(self):
+		"""Return the program description."""
 		description = (
 			"Welcome to DIFF LAB!\n\nTo start diffing, please enter the full names of the files you would like to "
 			"compare below (including the extensions).\n\nThe extensions currently supported are:"
@@ -67,12 +68,15 @@ class Differ:
 		return description
 
 	def show_error(self, message):
+		"""Show an error window with the appropriate message."""
 		messagebox.showerror(title="Error", message=message)
 
 	def show_diff_complete_info(self, message):
+		"""Show an information window stating that the diff is complete."""
 		messagebox.showinfo(title="Diff Complete", message=message)
 
 	def diff_files(self):
+		"""Open the files and diff them against each other."""
 		file_1_name = self.file_1_value.get()
 		file_2_name = self.file_2_value.get()
 		valid_file_names, extension = self.validate_file_names(file_1_name, file_2_name)
@@ -90,6 +94,7 @@ class Differ:
 		self.compare_files(file_1_path, file_2_path, extension)
 
 	def validate_file_names(self, file_1_name, file_2_name):
+		"""Validate the file names."""
 		if not file_1_name or not file_2_name:
 			self.show_error("You must provide both file names.")
 			return False, None
@@ -113,9 +118,11 @@ class Differ:
 		return True, file_1_ext
 
 	def get_file_path(self, file_name):
+		"""Get the full file path."""
 		return f"{CURRENT_PATH}\\{file_name}"
 
 	def validate_file_path(self, file_path):
+		"""Check that the file path exists."""
 		if not os.path.exists(file_path):
 			self.show_error(f"There is no file for the path '{file_path}'.")
 			return False
@@ -123,6 +130,7 @@ class Differ:
 		return True
 
 	def compare_files(self, file_1_path, file_2_path, extension):
+		"""Compare the two files for any differences."""
 		df_1 = self.open_file_as_dataframe(file_1_path, extension)
 		df_2 = self.open_file_as_dataframe(file_2_path, extension)
 
@@ -142,13 +150,16 @@ class Differ:
 		)
 
 	def open_file_as_dataframe(self, file_path, extension):
+		"""Open the file for the given file path as a dataframe."""
 		if extension == Extensions.XLSX.value:
 			return pd.read_excel(file_path, index_col=None, header=None)
 		elif extension == Extensions.CSV.value:
 			return pd.read_csv(file_path, index_col=None, header=None)
 
 	def validate_dataframe_structures(self, df_1, df_2):
+		"""Check that the two dataframes have the same structure (same number of columns and rows)."""
 		error_message = "Cannot compare files with different structures."
+
 		df_1_cols = df_1.shape[1]
 		df_2_cols = df_2.shape[1]
 
@@ -166,6 +177,12 @@ class Differ:
 		return True
 
 	def get_dataframe_differences(self, df_1, df_2):
+		"""Return the differences in the two dataframes.
+
+		The first return object should be a dataframe which contains values that only the first file has. The second
+		return object should be a dataframe which contains values that only the second file has. In both cases, if the
+		two files have the same cell value then it should be np.nan in the dataframe.
+		"""
 		columns = df_1.columns.tolist()
 		df_1_differences = {}
 		df_2_differences = {}
@@ -188,6 +205,11 @@ class Differ:
 		return pd.DataFrame.from_dict(df_1_differences), pd.DataFrame.from_dict(df_2_differences)
 
 	def create_diff_report_file(self, df_1_differences,  df_2_differences):
+		"""Create a .xlsx file containing the differences in the two files.
+
+		The first sheet should contain values that only the first file has. The second sheet should contain values that
+		only the second file has.
+		"""
 		df_1_differences = df_1_differences.style.applymap(lambda v: self.apply_background_colour(v))
 		df_2_differences = df_2_differences.style.applymap(lambda v: self.apply_background_colour(v))
 		timestamp = datetime.datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
@@ -198,10 +220,12 @@ class Differ:
 			df_2_differences.to_excel(writer, sheet_name="File 2 differences", header=False, index=False)
 
 	def apply_background_colour(self, value):
+		"""Apply colour to the cells (green if cell has no value and red if it does have a value)."""
 		colour = "green" if pd.isnull(value) else "red"
 		return f"background-color: {colour}"
 
 
+# Run this script to launch the diffing program.
 if __name__ == "__main__":
 	root = tk.Tk()
 	root.title("DIFF LAB")
