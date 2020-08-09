8dbf3e2de9696b8d030f08514f1a07dd4a21cf71
tinalimbu <tinalawati@gmail.com>
2020-08-09 17:00:41 +0100

    Added test framework

--- tests/test_diff_lab.py
+++ tests/test_diff_lab.py
@@ -1,1 +1,46 @@
-# TODO: edit
+from unittest import TestCase
+
+
+class GetDescriptionTestCase(TestCase):
+	# TODO: Add tests
+	pass
+
+
+class DiffFilesTestCase(TestCase):
+	# TODO: Add tests
+	pass
+
+
+class ValidateFileNamesTestCase(TestCase):
+	# TODO: Add tests
+	pass
+
+
+class GetFilePathTestCase(TestCase):
+	# TODO: Add tests
+	pass
+
+
+class ValidateFilePathTestCase(TestCase):
+	# TODO: Add tests
+	pass
+
+
+class CompareFilesTestCase(TestCase):
+	# TODO: Add tests
+	pass
+
+
+class ValidateDataframeStructuresTestCase(TestCase):
+	# TODO: Add tests
+	pass
+
+
+class GetDataframeDifferencesTestCase(TestCase):
+	# TODO: Add tests
+	pass
+
+
+class CreateDiffReportFileTestCase(TestCase):
+	# TODO: Add tests
+	pass
