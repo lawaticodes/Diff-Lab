16ac9b9ab5bd2178852b72c361e75ee384e59453
tinalimbu <tinalawati@gmail.com>
2020-08-17 21:51:30 +0100

    Fixed tests

--- tests/test_diff_lab_integration_tests.py
+++ tests/test_diff_lab_integration_tests.py
@@ -1,4 +1,5 @@
 import glob
+import numpy as np
 import os
 import pandas as pd
 import tkinter as tk
@@ -40,18 +41,37 @@ class DiffLabIntegrationTestCase(TestCase):
 		assert not self.get_output_files()
 
 	def test_xlsx_files_without_merged_cells_with_differences(self, mock_show_diff_complete_info):
-		expected_dataframe = pd.DataFrame()
 		self.compare_files(
 			"test_file_1_without_merged_cells.xlsx", "test_file_2_without_merged_cells.xlsx", Extensions.XLSX.value
+		)
+		expected_df_1_differences = pd.DataFrame(
+			{
+				0: [np.nan, np.nan, 11, np.nan, np.nan],
+				1: [2, np.nan, np.nan, 17, np.nan],
+				2: [np.nan, 8, np.nan, np.nan, np.nan],
+				3: [np.nan, 9, np.nan, np.nan, 24],
+				4: [np.nan, np.nan, np.nan, np.nan, 25],
+			}
+		)
+		expected_df_2_differences = pd.DataFrame(
+			{
+				0: [np.nan, np.nan, 110, np.nan, np.nan],
+				1: [20, np.nan, np.nan, 170, np.nan],
+				2: [np.nan, 80, np.nan, np.nan, np.nan],
+				3: [np.nan, 90, np.nan, np.nan, 240],
+				4: [np.nan, np.nan, np.nan, np.nan, 250],
+			}
 		)
 		output_files = self.get_output_files()
 
 		assert len(output_files) == 1
 
 		output_file_path = self.get_output_file_path(output_files[0])
-		dataframe = pd.read_excel(output_file_path, index_col=None, header=None)
+		df_1_differences = pd.read_excel(output_file_path, sheet_name=0, index_col=None, header=None)
+		df_2_differences = pd.read_excel(output_file_path, sheet_name=1, index_col=None, header=None)
 
-		assert dataframe.equals(expected_dataframe)
+		assert df_1_differences.equals(expected_df_1_differences)
+		assert df_2_differences.equals(expected_df_2_differences)
 
 	# def test_xlsx_files_with_merged_cells_without_differences(self, mock_show_diff_complete_info):
 	# 	self.differ.compare_files("", "", Extensions.XLSX.value)
